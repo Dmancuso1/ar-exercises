6621833932ecfc5b1728a1ed6092518bb9b823b1
Dane Mancuso <danegmancuso@gmail.com>
2020-08-17 23:33:52 +0000

    displays error message from ActiveRecord once new (error-driven) record is saved.

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,19 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+
+# Ask the user for a store name (store it in a variable)
+puts "create new store"
+@new_store = $stdin.gets.chomp
+
+
+# Attempt to create a store with the inputted name but leave out the other fields (annual_revenue, mens_apparel, and womens_apparel)
+store4 = Store.create(name: @new_store, created_at: Time.new, updated_at: Time.new)
+
+
+# Display the error messages provided back from ActiveRecord to the user (one on each line) after you attempt to save/create the record
+err = store4.errors.messages
+puts err
+err.each { |key, value| puts "#{key}: #{value[0]}"}
+
